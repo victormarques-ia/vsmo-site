0f8b05ced684f7b56c2a34cb3152f64562e163d9
Victor Silva <victormarques.ia@gmail.com>
2020-12-26 15:54:14 -0300

    create ExperiencesAndTechnologies component

---
+++ src/components/ExperiencesAndTechnologies/ExperiencesAndTechnologies.elements.tsx
@@ -0,0 +1,6 @@
+import { Container } from 'src/styles/global'
+import styled from 'styled-components'
+
+export const ExperiencesAndTechnologiesBody = styled.section``
+
+export const ExperiencesAndTechnologiesContainer = styled(Container)``
