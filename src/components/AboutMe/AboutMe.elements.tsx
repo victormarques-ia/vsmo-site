da5123e8e7a45481485f0bea7dbee57e0fdd80a9
Victor Silva <victormarques.ia@gmail.com>
2020-12-26 15:53:37 -0300

    create AboutMe component

---
+++ src/components/AboutMe/AboutMe.elements.tsx
@@ -0,0 +1,162 @@
+import Content from '@components/Content/Content'
+import styled from 'styled-components'
+import { Container } from '../../styles/global'
+
+export const AboutMeWrapper = styled.div`
+  position: relative;
+  overflow: hidden;
+
+  width: 100%;
+  height: 100vh;
+`
+
+export const AboutMeBody = styled.section`
+  display: flex;
+
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 12rem;
+`
+
+export const AboutMeContainer = styled(Container)`
+  display: grid;
+
+  grid-template-columns: min-content auto 37rem;
+  grid-template-rows: 67rem 2fr;
+
+  grid-template-areas:
+    'aboutMeTitleContent aboutMePhotoContainer aboutMeDescriptionContent'
+    'indicatorContainer indicatorContainer indicatorContainer';
+
+  @media (max-width: 1023px) {
+    align-items: center;
+    grid-template-columns: min-content auto;
+    grid-template-rows: auto auto 1fr 0.8fr;
+    grid-row-gap: 2rem;
+    grid-template-areas:
+      'aboutMeTitleContent aboutMePhotoContainer'
+      'aboutMeTitleContent aboutMePhotoContainer'
+      'aboutMeDescriptionContent aboutMeDescriptionContent'
+      'indicatorContainer indicatorContainer';
+  }
+`
+
+export const AboutMeTitleContent = styled(Content)`
+  grid-area: aboutMeTitleContent;
+
+  align-self: center;
+`
+export const AboutMeTitleContainer = styled.div``
+
+export const AboutMeTitle = styled.h1`
+  font: ${props => props.theme.fonts.detail};
+  font-size: 10.4rem;
+  line-height: 96.9%;
+  max-width: 70%;
+  @media (max-width: 500px) {
+    font-size: 6.4rem;
+  }
+`
+
+export const AboutMePhotoContainer = styled.div`
+  grid-area: aboutMePhotoContainer;
+
+  position: relative;
+  display: flex;
+  justify-content: center;
+
+  @media (max-width: 1023px) {
+    justify-content: flex-end;
+    align-items: center;
+  }
+`
+export const AboutMePhoto = styled.div`
+  position: absolute;
+  z-index: -5;
+
+  transition: all 0.5s ease;
+
+  svg {
+    width: 100%;
+    min-width: 45rem;
+    filter: grayscale(100%);
+  }
+
+  @media (max-width: 1023px) {
+    svg {
+      min-width: 40rem;
+    }
+  }
+
+  @media (max-width: 500px) {
+    svg {
+      min-width: 25rem;
+    }
+  }
+`
+
+export const AboutMeDescriptionContent = styled(Content)`
+  grid-area: aboutMeDescriptionContent;
+
+  align-self: center;
+`
+
+export const AboutMeDescriptionContainer = styled.div`
+  display: flex;
+  align-items: center;
+  white-space: pre-wrap;
+
+  @media (max-width: 1023px) {
+    justify-content: flex-end;
+    align-items: flex-end;
+  }
+`
+
+export const AboutMeDescription = styled.p`
+  font: ${props => props.theme.fonts.default};
+  @media (max-width: 1023px) {
+    max-width: 85%;
+  }
+`
+
+export const ArrowDown = styled.img`
+  display: none;
+`
+export const ScrollDown = styled.img``
+
+export const IndicatorContainer = styled.div`
+  grid-area: indicatorContainer;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  @media (max-width: 1023px) {
+    justify-content: center;
+
+    ${ArrowDown} {
+      display: initial !important;
+    }
+
+    ${ScrollDown} {
+      display: none !important;
+    }
+  }
+`
+
+export const BgContainer = styled.div`
+  position: fixed;
+
+  width: 100%;
+  height: 100%;
+  z-index: -50;
+
+  top: -8rem;
+`
+
+export const BgImage = styled.img`
+  position: absolute;
+
+  min-width: 100%;
+`
---
+++ src/components/AboutMe/AboutMe.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+
+import {
+  AboutMeBody,
+  AboutMeContainer,
+  AboutMeDescriptionContent,
+  AboutMeDescription,
+  AboutMeDescriptionContainer,
+  AboutMePhoto,
+  AboutMePhotoContainer,
+  AboutMeTitleContent,
+  AboutMeTitle,
+  AboutMeTitleContainer,
+  AboutMeWrapper,
+  BgContainer,
+  BgImage,
+  IndicatorContainer,
+  ScrollDown,
+  ArrowDown
+} from './AboutMe.elements'
+
+import Photo from '@static/photo.svg'
+
+const AboutMe: React.FC = () => {
+  return (
+    <AboutMeWrapper>
+      {/* <BgContainer>
+        <BgImage src="/purplebg_1.svg" />
+      </BgContainer> */}
+      <AboutMeBody>
+        <AboutMeContainer>
+          <AboutMeTitleContent title="developer" className="title">
+            <AboutMeTitleContainer>
+              <AboutMeTitle>Victor Silva</AboutMeTitle>
+            </AboutMeTitleContainer>
+          </AboutMeTitleContent>
+          <AboutMePhotoContainer>
+            <AboutMePhoto>
+              <Photo />
+            </AboutMePhoto>
+          </AboutMePhotoContainer>
+          <AboutMeDescriptionContent title="about me" className="description">
+            <AboutMeDescriptionContainer>
+              <AboutMeDescription>
+                I am a person passionate about challenges, and that has been
+                what moves me in a year of experience in software development.{' '}
+                {'\n \n'}I have experience in web and mobile development, for
+                this, using mainly React, React Native and Node.js.
+              </AboutMeDescription>
+            </AboutMeDescriptionContainer>
+          </AboutMeDescriptionContent>
+          <IndicatorContainer>
+            <ArrowDown src="/arrow_down.svg" />
+            <ScrollDown src="/scroll_down.svg" />
+          </IndicatorContainer>
+        </AboutMeContainer>
+      </AboutMeBody>
+    </AboutMeWrapper>
+  )
+}
+
+export default AboutMe
